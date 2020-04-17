a09fa2836081beafcd8accac706c7a5ffe20d714
upendra431 <uppu431@gmail.com>
2020-04-18 03:25:17 +0530

    ch1-2 Python anatomy

---
+++ ch1-2 Python anatomy/UsingMainMethod.py
@@ -0,0 +1,10 @@
+
+import platform
+def main():
+    message()
+def message():
+    print("This is Python version {} ".format(platform.python_version()))
+
+
+if __name__ == "__main__": main()
+    
