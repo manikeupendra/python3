581e0a2c014846e046da9beafa89cba37f3257ae
upendra431 <uppu431@gmail.com>
2020-04-18 22:48:55 +0530

    ch2-5 sequence types

---
+++ CH2 Types and Values/ch2-5 sequence types/dictionaryType.py
@@ -0,0 +1,4 @@
+di={1:"Hi",2:"Bye"}
+
+for k,v in di.items():
+    print(" Key and Value is {} :: {} ".format(k,v))
---
+++ CH2 Types and Values/ch2-5 sequence types/rangeStartWithEnd.py
@@ -0,0 +1,9 @@
+'''
+range(end)
+range(start,end)
+range(start,end,increment)
+'''
+x=range(2,10)
+
+for i in x:
+    print(i)
